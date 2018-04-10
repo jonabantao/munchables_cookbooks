97e1b127da66e21ddc5cb8aaf692dcd852442aea
Jonathan Abantao <jonabantao@gmail.com>
2018-04-09 20:28:45 -0700

    Add more secret keys for the cookbooks

--- munchables/attributes/default.rb
+++ munchables/attributes/default.rb
@@ -14,7 +14,11 @@ default[:munchables][:environment] = {
   "SECRET_KEY_BASE": node[:munchables][:secret_key_base],
   "DATABASE_URL": node[:munchables][:database_url],
   "RAILS_ENV": node[:munchables][:rails_env],
-  "RAILS_LOG_TO_STDOUT": node[:munchables][:log_to_stdout]
+  "RAILS_LOG_TO_STDOUT": node[:munchables][:log_to_stdout],
+  "s3_access_key_id": node[:munchables][:s3_access_key_id],
+  "s3_bucket": node[:munchables][:s3_bucket],
+  "s3_region": node[:munchables][:s3_region],
+  "s3_secret_access_key": node[:munchables][:s3_secret_access_key]
 }
 
 default[:munchables][:start_cmd] = "unicorn -E production -c /opt/unicorn.rb"
